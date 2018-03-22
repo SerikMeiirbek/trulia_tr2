a787dfff0c24cbe9035a02e014cb1936e81b85b2
sanjardoschanov <beckus83@gmail.com>
2018-03-22 11:21:27 -0400

    Ian's testcases

--- src/test/java/com/trulia/pages/ResultPage.java
+++ src/test/java/com/trulia/pages/ResultPage.java
@@ -12,97 +12,100 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.trulia.utilities.Driver;
 
 public class ResultPage {
-	
+
 	private WebDriver driver;
-	
+
 	public ResultPage() {
 		this.driver = Driver.getDriver();
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public WebDriver getDriver() {
 		return driver;
 	}
-	
+
 	@FindBy(id = "homeTypeToggle")
 	public WebElement homeType;
-	
+
 	@FindBy(id = "homeType1")
 	public WebElement Condo;
-	
+
 	@FindBy(tagName = "h2")
 	public WebElement h2;
-	
+
 	@FindBy(id = "homeType4")
 	public WebElement land;
-	
-	@FindBy(id = "bedroomsToggle") 
+
+	@FindBy(id = "bedroomsToggle")
 	public WebElement allBeds;
-	
-	@FindBy(xpath = "//button[text() = '2+']") 
+
+	@FindBy(xpath = "//button[text() = '2+']")
 	public WebElement moreThan2Beds;
-	
-	@FindBy(xpath = "//div[@id='bedroomsButtonGroup']//button") 
+
+	@FindBy(xpath = "//div[@id='bedroomsButtonGroup']//button")
 	public List<WebElement> bedOptions;
-	
-	@FindBy(xpath = "//div[@class='backgroundBasic']//li[1]") 
+
+	@FindBy(xpath = "//div[@class='backgroundBasic']//li[1]")
 	public List<WebElement> bedsResults;
-	
-	@FindBy(xpath = "//button[text() = '4+']") 
+
+	@FindBy(xpath = "//button[text() = '4+']")
 	public WebElement moreThan4Beds;
-	
-	@FindBy(className = "addressDetail") 
+
+	@FindBy(className = "addressDetail")
 	public List<WebElement> addressDetails;
-	
+
 	@FindBy(tagName = "h1")
 	public WebElement housrAddress;
-	
-	@FindBy(xpath = "//h3[@class='h3 mtn']") 
+
+	@FindBy(xpath = "//h3[@class='h3 mtn']")
 	public WebElement houseAddressText;
-	
-	@FindBy(xpath = "//ul[@class='listInlineBulleted man pts ptXxsHidden pbsXxsVisible']") 
+
+	@FindBy(xpath = "//ul[@class='listInlineBulleted man pts ptXxsHidden pbsXxsVisible']")
 	public WebElement houseDetialsText;
-	
+
 	@FindBy(tagName = "h2")
 	public WebElement notMatchText;
-	
-	@FindBy(xpath = "//h6[@class='typeLowlight']") 
+
+	@FindBy(xpath = "//h6[@class='typeLowlight']") // can we put just xpath = "//h6"?
 	public WebElement zeroResults;
+
+	@FindBy(id = "priceToggle")
+	public WebElement anyPriceButton;
 	
 	public int found() {
 		return Integer.parseInt(zeroResults.getText().substring(0, 1));
-		
+
 	}
-	
+
 	public boolean isAt() {
 		return h2.getText().contains("homes available on Trulia");
 	}
-	
+
 	public WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
 		WebDriverWait wait = new WebDriverWait(driver, timeToWaitInSec);
 		return wait.until(ExpectedConditions.visibilityOf(element));
 	}
-	
+
 	public int countOptions(List<WebElement> elements) {
 		return elements.size();
 	}
-	
+
 	public boolean countBeds(int number) {
-		for(WebElement element : bedsResults) {
+		for (WebElement element : bedsResults) {
 			String text = element.getText();
-			if(text.length() == 3) {
+			if (text.length() == 3) {
 				int i = Integer.parseInt(text.substring(0, 1));
 				if (i < number)
 					return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	public boolean verifyAutoSuggestions(String city) {
 		boolean result = false;
-		
+
 		for (WebElement element : addressDetails) {
 			if (element.getText().contains(city)) {
 				result = true;
@@ -110,5 +113,6 @@ public class ResultPage {
 		}
 		return result;
 	}
-
+	
+		
 }
---
+++ src/test/java/com/trulia/tests/functionaltest/SearchingHouses.java
@@ -0,0 +1,139 @@
+package com.trulia.tests.functionaltest;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.trulia.pages.ResultPage;
+import com.trulia.pages.TruliaPage;
+import com.trulia.utilities.BrowserUtils;
+import com.trulia.utilities.ConfigurationReader;
+import com.trulia.utilities.Driver;
+import com.trulia.utilities.TestBase;
+
+// Sanjar did Ian's TestCases:
+
+public class SearchingHouses extends TestBase {
+	WebDriver driver = Driver.getDriver();
+	TruliaPage trulia = new TruliaPage();
+	ResultPage resultPage = new ResultPage();
+	String city = "Gaithersburg";
+	String expected = "Montgomery Village";
+
+	@BeforeMethod
+	public void navigate() {
+		driver.get(ConfigurationReader.getProperty("url"));
+	}
+
+	@Test(priority = 0, description="TC005")
+	public void searchUsaOnly() {
+		assertTrue(trulia.isAt());
+		assertTrue(driver.getTitle().contains("Trulia"));
+		assertTrue(trulia.searchField.isDisplayed());
+		assertTrue(trulia.searchButton.isDisplayed());
+
+		trulia.searchField.clear();
+		trulia.searchField.sendKeys(city);
+		trulia.searchButton.click();
+		BrowserUtils.waitFor(2);
+		assertTrue(driver.getTitle().contains(city));
+
+		System.out.println(resultPage.verifyAutoSuggestions(city));
+	}
+
+	@Test(priority = 1, description="TC006")
+	public void selectMontgomery() {
+		assertTrue(trulia.isAt());
+		assertTrue(driver.getTitle().contains("Trulia"));
+		assertTrue(trulia.searchField.isDisplayed());
+		assertTrue(trulia.searchButton.isDisplayed());
+
+		trulia.searchField.clear();
+		trulia.searchField.sendKeys("Mon");
+		BrowserUtils.waitFor(2);
+
+		//NEED ATTENTION, this may not work always
+		driver.findElement(By.id("react-autowhatever-1--item-0")).click(); 
+		BrowserUtils.waitFor(2);
+		System.out.println(resultPage.verifyAutoSuggestions(expected));
+
+	}
+
+	@Test(priority = 2, description="TC007")
+	public void searchOutsideOfUsa() {
+
+		assertTrue(trulia.isAt());
+		assertTrue(driver.getTitle().contains("Trulia"));
+		assertTrue(trulia.searchField.isDisplayed());
+		assertTrue(trulia.searchButton.isDisplayed());
+
+		trulia.searchField.clear();
+		trulia.searchField.sendKeys("Khiva, Uzbekistan");
+		BrowserUtils.waitFor(2);
+		trulia.searchButton.click();
+		BrowserUtils.waitFor(2);
+
+		assertFalse(driver.getTitle().contains("LIMA, PERU"));
+		assertEquals(resultPage.zeroResults.getText(), "0 Results.");
+		assertEquals(resultPage.notMatchText.getText(), "Your search does not match any homes.");
+
+	}
+
+	@Test(priority = 3, description="TC008")
+	public void checkPrice() {
+		assertTrue(trulia.isAt());
+		assertTrue(driver.getTitle().contains("Trulia"));
+		assertTrue(trulia.searchField.isDisplayed());
+		assertTrue(trulia.searchButton.isDisplayed());
+
+		trulia.searchField.clear();
+		BrowserUtils.waitFor(2);
+		trulia.searchField.sendKeys(city);
+		trulia.searchButton.click();
+		BrowserUtils.waitFor(2);
+		assertTrue(driver.getTitle().contains(city));
+
+		assertTrue(resultPage.anyPriceButton.isDisplayed());
+		resultPage.anyPriceButton.click();
+
+		Select dropdown = new Select(driver.findElement(By.id("minPrice")));
+		dropdown.selectByVisibleText("$50k");
+		Select dropdown2 = new Select(driver.findElement(By.id("maxPrice")));
+		dropdown2.selectByVisibleText("$100k");
+		BrowserUtils.waitFor(2);
+
+		List<WebElement> qq = driver.findElements(By.xpath
+						("//span[@class='cardPrice h5 man pan typeEmphasize noWrap typeTruncate']"));
+		for (WebElement e : qq)
+			System.out.println(e.getText());
+		BrowserUtils.waitFor(2);
+		trulia.searchField.clear();
+
+		// missing last step here
+	}
+
+	@Test(priority = 4, description = "TC009")
+	public void lastVerification() {
+		assertTrue(trulia.isAt());
+		assertTrue(driver.getTitle().contains("Trulia"));
+		assertTrue(trulia.searchField.isDisplayed());
+		assertTrue(trulia.searchButton.isDisplayed());
+
+		trulia.searchField.clear();
+		trulia.searchField.sendKeys(city);
+		trulia.searchButton.click();
+		BrowserUtils.waitFor(2);
+		assertTrue(driver.getTitle().contains(city));
+
+	}
+
+}
