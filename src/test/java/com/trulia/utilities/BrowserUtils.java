0c3794eb5516106e41e6afed91282eb09b25b77c
Nodiraj <raytsentr@gmail.com>
2018-03-23 11:54:16 -0400

    changed Driver

--- src/test/java/com/trulia/utilities/BrowserUtils.java
+++ src/test/java/com/trulia/utilities/BrowserUtils.java
@@ -21,40 +21,38 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowserUtils {
-	private static WebDriver driver = Driver.getDriver();
-	
 	public static boolean waitForTextVisibility(WebElement element, int timeToWaitInSec, String text) {
-		WebDriverWait wait = new WebDriverWait(driver, timeToWaitInSec);
+		WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeToWaitInSec);
 		return wait.until(ExpectedConditions.textToBePresentInElement(element, text));
 	}
 	
 	public static List<WebElement> waitForVisibility(List<WebElement> elements, int timeToWaitInSec) {
-		WebDriverWait wait = new WebDriverWait(driver, timeToWaitInSec);
+		WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeToWaitInSec);
 		return wait.until(ExpectedConditions.visibilityOfAllElements(elements));
 	}
 
 	public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
-		WebDriverWait wait = new WebDriverWait(driver, timeToWaitInSec);
+		WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeToWaitInSec);
 		return wait.until(ExpectedConditions.visibilityOf(element));
 	}
 
 	public static WebElement waitForVisibility(By locator, int timeout) {
-		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeout);
 		return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 	}
 
 	public static WebElement waitForClickablility(WebElement element, int timeout) {
-		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeout);
 		return wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
 
 	public static WebElement waitForClickablility(By locator, int timeout) {
-		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeout);
 		return wait.until(ExpectedConditions.elementToBeClickable(locator));
 	}
 
 	public static WebElement fluentWait(final WebElement webElement, int timeinsec) {
-		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(timeinsec, TimeUnit.SECONDS)
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(Driver.getDriver()).withTimeout(timeinsec, TimeUnit.SECONDS)
 				.pollingEvery(timeinsec, TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
 		WebElement element = wait.until(new Function<WebDriver, WebElement>() {
 			public WebElement apply(WebDriver driver) {
@@ -72,7 +70,7 @@ public class BrowserUtils {
 		};
 		try {
 			System.out.println("Waiting for page to load...");
-			WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+			WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeOutInSeconds);
 			wait.until(expectation);
 		} catch (Throwable error) {
 			System.out.println(
@@ -89,24 +87,24 @@ public class BrowserUtils {
 	}
 
 	public static void switchToWindow(String targetTitle) {
-		String origin = driver.getWindowHandle();
-		for (String handle : driver.getWindowHandles()) {
-			driver.switchTo().window(handle);
-			if (driver.getTitle().equals(targetTitle)) {
+		String origin = Driver.getDriver().getWindowHandle();
+		for (String handle : Driver.getDriver().getWindowHandles()) {
+			Driver.getDriver().switchTo().window(handle);
+			if (Driver.getDriver().getTitle().equals(targetTitle)) {
 				return;
 			}
 		}
-		driver.switchTo().window(origin);
+		Driver.getDriver().switchTo().window(origin);
 	}
 
 	public static void getScreenShot(String fileName) throws IOException {
-		File outputFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File outputFile = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(outputFile, new File(System.getProperty("user.dir")
 				+ "//src//test//java//com//companyname//projectname//screenshots//" + fileName + ".jpg"));
 	}
 
 	public static List<String> getElementsText(By locator) {
-		List<WebElement> elems = driver.findElements(locator);
+		List<WebElement> elems = Driver.getDriver().findElements(locator);
 		List<String> elemTexts = new ArrayList<>();
 		
 		for (WebElement el : elems) {
